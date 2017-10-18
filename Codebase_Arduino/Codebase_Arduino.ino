5449aa4cf554272a458daaad166eeccb2db84816
Rokcky Petkov <rocky@weeklybeat.org>
2017-10-17 23:19:46 -0400

    No Mic Code

--- Codebase_Arduino/Codebase_Arduino.ino
+++ Codebase_Arduino/Codebase_Arduino.ino
@@ -21,6 +21,7 @@
 #include <SPI.h>
 #include <Adafruit_ATParser.h>
 
+#include "Codebase_Arduino.h"
 #include "BluefruitConfig.h"
 
 #if SOFTWARE_SERIAL_AVAILABLE
@@ -31,23 +32,22 @@
 #define FACTORYRESET_ENABLE 1 // Allows us to do a factory reset to put device in known good state!
 #define FFT_N 128             // Not really using the library so i'll just define a constant to save memory.
 #define MIC_ADC_CHANNEL 6     // Testing this with ADC Channel 9 (Note: Unable to find mapping for Flora Series)
-#define PIXEL_PIN 11          // Neo Pixels are plugged into the D11 pin
 
+#define PIXEL_PIN 12          // Neo Pixels are plugged into the D11 pin
 #define NUM_COLOURS 1         // We display a maximum of one colour at a time. 
+#define PIXEL_COUNT 24        // Number of pixels on our ring!
 
-// TODO: Definethe data in and data out pins as constants. 
-int16_t capture[FFT_N];                   // Audio Capture Buffers
-volatile byte samplePos = 0;              // Position Counter for the Buffer
+#define BLUEFRUIT_HWSERIAL_NAME Serial1
 
 // A couple globals which are useful for processing incomming colours. 
 const char *delimiters = {"[,]"};    // Delimiters for splitting strings
-char       *pch;                            // Pointer to the lead character in our string
+char       *pch;                     // Pointer to the lead character in our string
+uint8_t colour[3 * NUM_COLOURS] = {0, 0, 0};    // Three values for each colour. Initialise it to 0.
 
-// TODO: NeoPixel Variables
-uint8_t colour[3 * NUM_COLOURS] = {0, 0, 0};    // Three values for each colour. Initialise it to 0. 
-
+// Initialise our NeoPixel object
+Adafruit_NeoPixel ring = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+ 
 // Some Basic Bluetooth Things
-#define BLUEFRUIT_HWSERIAL_NAME           Serial1
 Adafruit_BluefruitLE_UART ble(BLUEFRUIT_HWSERIAL_NAME, BLUEFRUIT_UART_MODE_PIN);
 
 /*
@@ -65,13 +65,14 @@ Adafruit_BluefruitLE_UART ble(BLUEFRUIT_HWSERIAL_NAME, BLUEFRUIT_UART_MODE_PIN);
  * when we are doing it over a network. 
  */
 void setup() {
+   char       *pch;                            // Pointer to the lead character in our string
 
    // Waiting for the serial link to be established
    while(!Serial);
    delay(500);
   
    // Start Serial communications so we can do output. 
-   Serial.begin(9600);
+   Serial.begin(115200);
    Serial.println(F("Welcome to the Rave Light Show!"));
    
   // Factory reset the bluetooth module so that everything is in a safe place
@@ -83,7 +84,7 @@ void setup() {
   
   Serial.println( F("OK!") );
 
-  if ( FACTORYRESET_ENABLE )
+  if (FACTORYRESET_ENABLE)
   {
     Serial.println("Resetting Bluetooth Module:");
     if (!ble.factoryReset()) {
@@ -97,27 +98,18 @@ void setup() {
   Serial.println("Requesting Bluefruit info:");
   ble.info();
 
+   Serial.println(F("Initialising the lights"));
+  ring.begin();
+  ring.show();
+
   // Wait for the incomming bluetooth connexion
   Serial.println("Waiting for connection...");
   
-//  while (! ble.isConnected()) {
-//    Serial.println(F("Stallin like Stalin!"));
-//    delay(500);
-//  }
+  while (! ble.isConnected()) {
+    Serial.println(F("Stallin like Stalin!"));
+    delay(500);
+  }
 
-  Serial.println("Enabling Free Run Mode");
-  // Init ADC free-run mode; f = ( 16MHz/prescaler ) / 13 cycles/conversion 
-  ADMUX  = MIC_ADC_CHANNEL; // Channel sel, right-adj, use AREF pin
-  ADCSRA = _BV(ADEN)  | // ADC enable
-           _BV(ADSC)  | // ADC start
-           _BV(ADATE) | // Auto trigger
-           _BV(ADIE)  | // Interrupt enable
-           _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0); // 128:1 / 13 = 9615 Hz ((SAMPLE RATE))
-  ADCSRB = 0;                // Free run mode, no high MUX bit
-  DIDR0  = 1 << MIC_ADC_CHANNEL; // Turn off digital input for ADC pin
-  TIMSK0 = 0;                // Timer0 off
-
-  sei();  // Enabling interrupts. 
 
   Serial.println("\n\nBeginning Main Loop...");
 }
@@ -144,73 +136,47 @@ void setup() {
 void loop() {
   
   int i, j;                                       // Loop counters
-  uint8_t bytesRead;                              // No. of bytes read to the buffer
-
-  
-  while(ADCSRA & _BV(ADIE));  // Wait for audio sampling to finish
-  samplePos = 0;              // Reset Sample Counter
-  ADCSRA |= _BV(ADIE);        // Resumes the sampling interrupt
-
-  // Temporary for loop to read frequencies.
-  for (i = 0; i < FFT_N; i++) {
-    Serial.println(capture[i], DEC);
+  // Check for incoming characters from Bluefruit
+  ble.println("AT+BLEUARTRX");
+  ble.readline();
+  Serial.println(ble.buffer);
+  if (strcmp(ble.buffer, "OK") == 0) {
   }
-  
-//  // Check for incoming characters from Bluefruit
-//  ble.println("AT+BLEUARTRX");
-//  ble.readline();
-//  if (strcmp(ble.buffer, "OK") == 0) {
-//    // no data
-//  }
-//    else {
-//    // Converting byte string into integers
-//    pch = strtok(ble.buffer, delimiters);
-//    i = 0;
-//    // While loop terminates when we run out of tokens or we have read more than 3 tokens
-//    while (pch != NULL || i < (3 * NUM_COLOURS)) {
-//      colour[i] = atoi(pch);    // Translate pch into an int and store in colour
-//      Serial.println(colour[i], HEX);
-//      pch = strtok(NULL, delimiters);
-//      i++;
-//    }
-//    ble.waitForOK();
-//  }
-//
-//  Serial.println("Sending...");
-//  // Sending one number at a time. 
-//  for (j = 0; j < FFT_N; j++) {
-//    ble.print("AT+BLEUARTTX=");
-//    ble.print(capture[j]);    // Print the ith value of capture
-//    ble.println(",");           // Add in a comma for easy parsing on the python end. 
-//
-//    // Waiting to ensure that we are okay w.r.t. seding our data
-//    if (! ble.waitForOK()) {
-//    Serial.println(F("Failed to send buffer."));
-//    }
-//  }
+  else {
+      
+      // Converting byte string into integers
+      pch = strtok(ble.buffer, delimiters);
+      i = 0;
 
+     
+      // While loop terminates when we run out of tokens or we have read more than 3 tokens
+      while (pch != NULL && i < (3 * NUM_COLOURS)) {
+        colour[i] = atoi(pch);    // Translate pch into an int and store in colour
+        //Serial.println(pch);
+        pch = strtok(NULL, delimiters);
+        //Serial.println(colour[i]);
+        i++;
+      }
+    ble.waitForOK();
+    }
 
-}
+   if ((colour[0] | colour[1] | colour[2])) {
+     blockPattern(colour);  
+   }   
+}  
 
 
 /*
- * Below is an interrupt service routine
- * which takes 128 audio samples and normalises them
- * around 0 for the DFT.
+ * The simplest pattern. Naively displays the same colour 
+ * for all of the lights in the ring.
  */
-ISR(ADC_vect) { 
-    static const int16_t noiseThreshold = 4;  // We can play around with this. We don't want to have lights run on ambient nothing!
-    int16_t sample = ADC; // Raw voltage over the wire. 0 corresponds to 0 volts. 1023 corresponds to ~5v.
-    
-    // Normalise our samples around 0 for the DFT
-    capture[samplePos] = 
-      ((sample > (512 - noiseThreshold)) &&
-      sample < (512 + noiseThreshold)) ? 0 : 
-      sample - 512;  
-
-      // Here we check if our buffer is full. If so we turn off our interrupt. 
-      if(++samplePos >= FFT_N) {
-          ADCSRA &= ~_BV(ADIE);
-      }
+void blockPattern(uint8_t *colour) {
+  int light;
+  for(light = 0; light < PIXEL_COUNT; light++) {
+    // Set the ring colour
+    ring.setPixelColor(light, colour[0], colour[1], colour[2], 255);
+  }
+  ring.show();  // And latch them in!
 }
+
 
