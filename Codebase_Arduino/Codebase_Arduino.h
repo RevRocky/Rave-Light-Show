723237d0d8e5abaed5297e1b78d60b6f01467759
Rokcky Petkov <rocky@weeklybeat.org>
2017-10-17 23:19:59 -0400

    Up to Date arduino

---
+++ Codebase_Arduino/Codebase_Arduino.h
@@ -0,0 +1,8 @@
+/*
+ * Below are declarations for the different colour patterns
+ */
+
+/*
+ * A simple pattern which has all lights display the given colour
+ */
+void blockPattern(int8_t *colour);
